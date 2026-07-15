e89451b76860a0370e89ffb2c98e65aeabce91aa
ImSumbul <sumbul0224@gmail.com>
2026-07-15 06:56:54 -0700

    made connection.php dynamic

--- admin/partials/connection.php
+++ admin/partials/connection.php
@@ -1,25 +1,9 @@
 <?php
-
-$env = __DIR__ . '/../.env';
 session_start();
-
-if (file_exists($env)) {
-    $lines = file($env, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
-            continue;
-        }
-
-        list($key, $value) = explode('=', $line, 2);
-        $_ENV[trim($key)] = trim($value);
-    }
-}
-
-define('LOCALHOST', $_ENV['DB_HOST']);
-define('DB_USERNAME', $_ENV['DB_USERNAME']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_NAME', $_ENV['DB_DATABASE']);
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','my-secret-pw');
+define('DB_NAME','foodStall');
 
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD,  DB_NAME) or die(mysqli_connect_error());
 ?>
