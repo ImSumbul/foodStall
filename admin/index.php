0107ff196dbd7a1ec9784e021efb638e75f3a4d2
ImSumbul <sumbul0224@gmail.com>
2026-07-20 07:40:12 -0700

    added category count from database.

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,18 @@
 <?php include_once('partials/menu.php'); ?>
+<?php
+$sql = "select COUNT(1) cnt from category";
+$result = $conn->query($sql);
+
+$category_count = $result->fetch_assoc()['cnt'];
+?>
+
 
     <!-- Main content section Starts here  -->
     <div class="main-content">
         <div class="wrapper">
             <h1>Dashboard</h1>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <h1><?= $category_count ?></h1>
                 <br/>
                 Categories
             </div>
